1840256649df9ac0504dc3b59d8529fef37eb1aa
TaiTranDang4150 <trandangtai1405@gmail.com>
2025-09-26 12:47:31 +0700

    change local database to cloud database

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://pet_shop_user:TaiLoK@localhost:5432/pet_shop"
+DATABASE_URL = "postgresql://neondb_owner:npg_IK7xeaJuZ2fD@ep-empty-band-a1hvwiw6-pooler.ap-southeast-1.aws.neon.tech/ users?sslmode=require&channel_binding=require"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
